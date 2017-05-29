a34361b3192ecd87c09a446580e3c3ef926f8a41
EzequielLuppino <ezequiel_luppino@live.com>
2017-05-29 17:33:09 -0300

    Diseño más apropiado de la creación

--- src/main/java/model/Indicador.java
+++ src/main/java/model/Indicador.java
@@ -10,5 +10,28 @@ public class Indicador {
 	public Double obtenerValor(String empresa, String periodo){
 		return 0.0;
 	}
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getFormula() {
+		return formula;
+	}
+
+	public void setFormula(String formula) {
+		this.formula = formula;
+	}
 }
