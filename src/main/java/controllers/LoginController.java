64a0d992f7f0b7ab9312e43a660b44f2eb9125c0
Nahuel <nahuefederico@gmail.com>
2017-10-10 23:17:36 -0300

    Commit inicial con página de login

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+import spark.ModelAndView;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+
+public class LoginController implements WithGlobalEntityManager, TransactionalOps{
+	/*
+	public ModelAndView mostrar(Request req, Response res){
+		req.queryParams();
+		
+		Map<String, Proyecto> model = new HashMap<>();
+		String id = req.params("id");
+		
+		Proyecto proyecto = RepositorioProyectos.instancia.buscar(Long.parseLong(id));
+		model.put("proyecto", proyecto);
+		return new ModelAndView(model, "proyectos/show.hbs");
+	}*/
+}
