d1f1b7cdd95ceb767b1840c73f453cdb706e5fd9
Mazzeo Alejandro <mazzeo.alejandro@gmail.com>
2017-11-17 17:35:52 -0300

    Modificacion de FileUpload para que suba varios archivos sin especificar el nombre

--- src/main/java/batchProcesses/FileUpload.java
+++ src/main/java/batchProcesses/FileUpload.java
@@ -10,33 +10,38 @@ import useCases.CuentasUseCases;
 
 public class FileUpload extends TimerTask {
 
-	final static String rutaYNombre = "./WatchFolder Carga/ArchivoDeCarga.txt";
+	final static String rutaCarga = "./WatchFolder Carga/";
 	final static String rutaProcesado = "./WatchFolder Carga/Procesados/";
-	
+
 	public void run() {
-		
-		File fileCarga = new File(rutaYNombre);
-		if(fileCarga.exists()) {
-			System.out.println("Archivo cargado");
-			CuentasUseCases.cargarArchivoDeCuentas(rutaYNombre);		 
-			fileCarga.renameTo(new File(rutaProcesado + getCurrentDate("yyyyMMddHHmmss") + "ArchivoCargado.txt"));		
-		}	
+
+		File folderCarga = new File(rutaCarga);
+		File[] listOfFiles = folderCarga.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {				
+				CuentasUseCases.cargarArchivoDeCuentas(rutaCarga + listOfFiles[i].getName());
+				listOfFiles[i].renameTo(
+						new File(rutaProcesado + getCurrentDate("yyyyMMddHHmmsss") + listOfFiles[i].getName()));
+				System.out.println("Archivo cargado");
+			}
+		}
 	}
 
 	public void init() {
 		Timer timer = new Timer();
 
-		timer.scheduleAtFixedRate(this, 0, 10*1000);
+		timer.scheduleAtFixedRate(this, 0, 10 * 1000);
 
 	}
-	
+
 	public static String getCurrentDate(String format) {
-	    String dtStr = "";
-	    SimpleDateFormat sdf = new SimpleDateFormat(format);
-	    Date dt1 = new Date();
+		String dtStr = "";
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		Date dt1 = new Date();
 
-	    dtStr = sdf.format(dt1);
+		dtStr = sdf.format(dt1);
 
-	    return dtStr;
+		return dtStr;
 	}
 }
